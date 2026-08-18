b93c56b853250bdf3bd90b32721c169028250911
SAGorev <SAGorev@greenatom.ru>
2026-08-18 16:25:54 +0500

    Ref.#18_08_26

---
+++ src/test/java/WebDriverHeadlessTest.java
@@ -0,0 +1,50 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WebDriverHeadlessTest {
+    WebDriver driver;
+
+    // Выносим URL в константу (в идеале - из config/properties)
+    private static final String BASE_URL = System.getProperty("test.url",
+            "https://otus.home.kartushin.su/training.html");
+
+    @BeforeAll
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Современный headless режим
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit(); // quit вместо close закрывает все окна и драйвер
+        }
+    }
+
+    @Test
+    public void testTextInputInHeadlessMode() {
+        driver.get(BASE_URL);
+
+        var inputField = driver.findElement(By.id("textInput"));
+        inputField.sendKeys("ОТУС");
+
+        String actualText = inputField.getAttribute("value");
+        Assertions.assertEquals("ОТУС", actualText,
+                "Текст не соответствует введенному: ОТУС!");
+    }
+}
---
+++ src/test/java/WebDriverMaximizedModeTest.java
@@ -0,0 +1,66 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WebDriverMaximizedModeTest {
+    WebDriver driver;
+
+    private static final String BASE_URL = System.getProperty("test.url",
+            "https://otus.home.kartushin.su/training.html");
+
+    @BeforeAll
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testFormSubmissionInMaximizedMode() {
+        driver.get(BASE_URL);
+
+        String testName = "Сергей";
+        String testEmail = "asdf@sdfg.rt";
+
+        WebElement nameInput = driver.findElement(By.id("name"));
+        WebElement emailInput = driver.findElement(By.id("email"));
+
+        nameInput.sendKeys(testName);
+        emailInput.sendKeys(testEmail);
+
+        WebElement submitBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        submitBtn.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement successMessage = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.className("message"))
+        );
+
+        String expectedMessage = String.format(
+                "Форма отправлена с именем: %s и email: %s", testName, testEmail
+        );
+
+        Assertions.assertEquals(expectedMessage, successMessage.getText(),
+                "Сообщение об успешной отправке не соответствует ожидаемому");
+    }
+}
